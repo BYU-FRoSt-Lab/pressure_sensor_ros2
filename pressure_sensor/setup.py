977ec787ba7fe7521d2e899c22f61d13d2703177
Braden Meyers <bjz-github@spwiz.com>
2026-01-09 14:30:57 -0700

    pressure sensor update

--- pressure_sensor/setup.py
+++ pressure_sensor/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'pressure_sensor'
 
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,6 +26,7 @@ setup(
     entry_points={
         'console_scripts': [
             'pressure_pub = pressure_sensor.pressure_pub:main',
+            'pressure_to_depth = pressure_sensor.pressure_to_depth:main',
         ],
     },
 )
